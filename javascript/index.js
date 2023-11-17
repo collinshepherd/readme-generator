763c4f3679e13b47dfe95068e2e98f92a6fbb0bd
collinshepherd <77903396+collinshepherd@users.noreply.github.com>
2023-11-16 18:33:53 -0600

    Added a function that provides the correct license badge

--- javascript/index.js
+++ javascript/index.js
@@ -17,6 +17,7 @@ const questions = [
     type: "input",
     name: "installation",
     message: "Enter instructions on how to install your project:",
+    default: "npm i",
   },
   {
     type: "input",
@@ -37,7 +38,17 @@ const questions = [
     type: "list",
     name: "license",
     message: "Choose a license for your project:",
-    choices: ["MIT", "GPL-3.0", "MPL-2.0", "Apache-2.0", "MPL-2.0", "None"],
+    choices: ["MIT", "GPL-3.0", "MPL-2.0", "Apache-2.0", "None"],
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your github username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email:",
   },
 ];
 
@@ -56,8 +67,9 @@ ${answers.description}
 - [License](#license) 
 
 ## Installation
+${"```"}
 ${answers.installation}
-
+${"```"}
 ## Usage
 ${answers.usage}
 
@@ -65,10 +77,17 @@ ${answers.usage}
 ${answers.contribution}
 
 ## Tests
-${answers.test}
+${answers.tests}
 
 ## License
-${answers.license}`;
+${generateLicense(answers.license)}
+
+## Questions
+[GitHub](https://github.com/${answers.github})
+
+[Repo Link](https://github.com/${answers.github}/${answers.title})
+
+For any questions reach out to me at ${answers.email}`;
 }
 
 inquirer.prompt(questions).then((answers) => {
@@ -76,3 +95,23 @@ inquirer.prompt(questions).then((answers) => {
     err ? console.error(err) : console.log("It worked!")
   );
 });
+
+function generateLicense(license) {
+  switch (license) {
+    case "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "GPL-3.0":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "MPL-2.0":
+      return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
+      break;
+    case "Apache-2.0":
+      return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "None":
+      return "";
+      break;
+  }
+}
